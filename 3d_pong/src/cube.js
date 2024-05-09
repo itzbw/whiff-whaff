c06ea2d85421cc7864ca4c37d98af3da738daa47
bw <b@bo>
2024-05-10 00:18:44 +0200

    3d

--- 3d_pong/src/cube.js
+++ 3d_pong/src/cube.js
@@ -15,13 +15,16 @@ import * as THREE from 'three';
 
 export class Cube extends THREE.Mesh {
   constructor() {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    super(geometry, material)
+    const geometry = new THREE.SphereGeometry(1, 32, 16);
+    const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+    super(geometry, material);
+    this.castShadow = true;
+    this.receiveShadow = false;
   }
 
   animate() {
     this.rotation.x += 0.01;
     this.rotation.y += 0.01;
+    this.rotation.z += 0.01;
   }
 }
--- 3d_pong/src/game.js
+++ 3d_pong/src/game.js
@@ -1,22 +1,33 @@
 import * as THREE from 'three';
-import { Cube } from './cube'
+//import { Cube } from './cube'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 50;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const cube = new Cube()
-scene.add(cube);
+const geometry = new THREE.SphereGeometry(15, 32, 16);
+const material = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load('./assets/moon.jpg')
+    //color: 0x3268F8
+});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
-camera.position.set(0, 0, 5)
+
 
 function animate() {
     requestAnimationFrame(animate);
-    renderer.render(scene, camera);
 
-    cube.animate()
-}
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+};
+
+
+
 animate();
