3bde471814c611dfd62837f6396fe65af94f35b8
bw <b@bo>
2024-05-26 21:23:14 +0200

    naturalMesh

--- rendering/main.js
+++ rendering/main.js
@@ -1,9 +1,11 @@
 //import "./cube.js"
 
-import "./sphere.js"
+//import "./sphere.js"
 
 //import "./smoke.js"  
 
 //import "./galaxy_sphere.js"
 
 //import "./galaxy.js"
+
+import "./naturalMesh.js"
